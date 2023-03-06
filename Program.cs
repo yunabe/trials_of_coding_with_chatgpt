cb34fd285d7847c14a55a8db1e9e7adc856baf0e
Yu Watanabe <yunabe.public@gmail.com>
2023-03-06 12:52:40 +0000

    Program.cs by ChatGPT

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var inputFile = "";
+        var outputFile = "";
+
+        // Parse command line arguments
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-i" && i + 1 < args.Length)
+            {
+                inputFile = args[i + 1];
+            }
+            else if (args[i] == "-o" && i + 1 < args.Length)
+            {
+                outputFile = args[i + 1];
+            }
+        }
+
+        // Ensure required arguments are set
+        if (string.IsNullOrEmpty(inputFile) || string.IsNullOrEmpty(outputFile))
+        {
+            Console.WriteLine("Usage: Compiler -i <input file> -o <output file>");
+            return;
+        }
+
+        // Read input file
+        var sourceCode = File.ReadAllText(inputFile);
+
+        // Create lexer and tokenize input
+        var lexer = new Lexer();
+        var tokens = lexer.Tokenize(sourceCode);
+
+        // Create parser and generate parse tree
+        var parser = new Parser();
+        var rootNode = parser.Parse(tokens);
+
+        // Create code generator and generate code
+        var codeGenerator = new CodeGenerator();
+        var generatedCode = codeGenerator.GenerateCode(rootNode);
+
+        // Write compiled code to output file
+        File.WriteAllText(outputFile, generatedCode);
+
+        Console.WriteLine("Compilation successful!");
+    }
+}
